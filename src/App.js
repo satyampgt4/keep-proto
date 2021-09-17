be44b3fe5dcef69db9a54df6f4d1668a6267b86a
satyampgt4 <satyamgpt44@gmail.com>
2021-09-17 13:07:14 +0530

    local storage

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import Header from "./Components/Header";
 import CreateArea from "./Components/CreateArea";
 import Note from "./Components/Note";
-import Footer from './Components/Footer';
+import Footer from "./Components/Footer";
 function App() {
-  const[notes, setNotes] = useState([]);
-  const addNote = (newNotes) =>{
-    setNotes((oldValue) =>
-    {
-      return [...oldValue, newNotes];
-      
+  const [notes, setNotes] = useState([]);
+
+  window.onload = function () {
+    var temp = JSON.parse(window.localStorage.getItem("noteslocal"));
+    if (temp != null) {
+      setNotes(() => {
+        return temp;
+      });
+    }
+  };
+
+  const addNote = (newNotes) => {
+    setNotes((oldValue) => {
+      var newValue = [...oldValue, newNotes];
+      window.localStorage.setItem("noteslocal", JSON.stringify(newValue));
+      return newValue;
     });
-  }
-  const deleteNote = (id) =>{
-    setNotes((oldValue) =>{
-      return [...oldValue.filter((note,index) =>
-      index !== id)];
-      
+  };
+  const deleteNote = (id) => {
+    setNotes((oldValue) => {
+      var newValue = [...oldValue.filter((note, index) => index !== id)];
+      window.localStorage.setItem("noteslocal", JSON.stringify(newValue));
+      return newValue;
     });
-  }
-  return(
+  };
+  return (
     <>
-    <Header/>
-    <div className ="Notearea">
-    <CreateArea onAdd = {addNote}/>
-    {notes.map((note, index) =>
-    {return(
-      <Note 
-        key = {index}
-        id = {index}
-        title = {note.title}
-        content = {note.content}
-        onDelete ={deleteNote}/>) ;
-    })}
-    </div>
-    <Footer/>
+      <Header />
+      <div className="Notearea">
+        <CreateArea onAdd={addNote} />
+        {notes.map((note, index) => {
+          return (
+            <Note
+              key={index}
+              id={index}
+              title={note.title}
+              content={note.content}
+              onDelete={deleteNote}
+            />
+          );
+        })}
+      </div>
+      <Footer />
     </>
   );
 }
